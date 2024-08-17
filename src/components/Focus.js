3f3dea0876af86e14e870f32854079b7b83729da
frances9172000 <francesgonzales822@gmail.com>
2024-08-17 09:18:20 +0800

    Updated the focus section

--- src/components/Focus.js
+++ src/components/Focus.js
@@ -1,44 +1,52 @@
+import ResponsiveIcon from "../../public/responsive.png";
+import SimplicityIcon from "../../public/simplicity.png";
+import CompatibilityIcon from "../../public/compatibility.png";
+import Image from "next/image";
+import { useInView } from "framer-motion";
+
 export default function Focus() {
+
   return (
     <div className="md:flex flex-wrap justify-evenly">
       <div className="card w-full md:w-[250px] my-3 mx-auto justify-between text-slate-400 text-sm text-center ">
-        <div className="bg-[var(--secondary)] p-5">
+        <div className="bg-[var(--secondary)] p-5 h-full">
+          <div className="h-[200px] p-4">
+            <Image alt="responsive icon" src={ResponsiveIcon} />
+          </div>
+          <h1 className="mb-3 text-2xl text-[var(--secondary)]">
+            Responsiveness
+          </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            aliquid nostrum reprehenderit repudiandae, dolorum eius iure
-            dignissimos aliquam, officiis laudantium facere, architecto ad
-            adipisci. Nobis, officiis ullam corrupti possimus explicabo,
-            voluptates alias perferendis voluptate, natus officia veniam nam ut
-            pariatur facilis mollitia amet ipsam accusantium eius eos dolores
-            temporibus fugiat?
+            Fits in any device. Mobile-first development, that's one of my main
+            objectives in creating a website.
           </p>
         </div>
       </div>
 
       <div className="card w-full md:w-[250px] my-3 mx-auto justify-between text-slate-400 text-sm text-center ">
-        <div className="bg-[var(--secondary)] p-5">
+        <div className="bg-[var(--secondary)] p-5 h-full">
+          <div className="h-[200px] p-4">
+            <Image alt="responsive icon" src={SimplicityIcon} />
+          </div>
+          <h1 className="mb-3 text-2xl text-[var(--secondary)]">Simplicity</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            aliquid nostrum reprehenderit repudiandae, dolorum eius iure
-            dignissimos aliquam, officiis laudantium facere, architecto ad
-            adipisci. Nobis, officiis ullam corrupti possimus explicabo,
-            voluptates alias perferendis voluptate, natus officia veniam nam ut
-            pariatur facilis mollitia amet ipsam accusantium eius eos dolores
-            temporibus fugiat?
+            Simplicity for beauty. Simple without any complexity will create
+            harmony to users and clients.
           </p>
         </div>
       </div>
 
-      <div className="card w-full md:w-[250px] my-3 mx-auto justify-between text-slate-400 text-sm text-center ">
-        <div className="bg-[var(--secondary)] p-5">
+      <div className="card w-full md:w-[250px] my-3 mx-auto justify-between text-slate-400 text-sm text-center">
+        <div className="bg-[var(--secondary)] p-5 h-full">
+          <div className="h-[200px] p-4">
+            <Image alt="responsive icon" src={CompatibilityIcon} />
+          </div>
+          <h1 className="mb-3 text-2xl text-[var(--secondary)]">
+            Compatibility
+          </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            aliquid nostrum reprehenderit repudiandae, dolorum eius iure
-            dignissimos aliquam, officiis laudantium facere, architecto ad
-            adipisci. Nobis, officiis ullam corrupti possimus explicabo,
-            voluptates alias perferendis voluptate, natus officia veniam nam ut
-            pariatur facilis mollitia amet ipsam accusantium eius eos dolores
-            temporibus fugiat?
+            Works in any browser. Cross-browser compatiblity of any site is a
+            must, that's why I equiped my projects with browser-friendly codes.
           </p>
         </div>
       </div>
